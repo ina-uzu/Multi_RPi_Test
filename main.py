ff19d6ac1c5b2f068d3849371bf1dba45b4c4f50
ina-uzu <ina.uzu.song@gmail.com>
2019-02-01 06:37:55 +0000

    add ph-device and remove unused code

--- main.py
+++ main.py
@@ -19,51 +19,51 @@ from brix import BRIX
 from kafka import KafkaProducer
 import simplejson as json
 
-
 DEVICE_ID = 1
 SERVER_ADR = 'localhost:9092'
 TOPIC_NAME = 'hello-kafka'
 
-device = AtlasI2C() 	# creates the I2C port object, specify the address or bus if necessary
-device_dco2 = DCO2()
-device_brix = BRIX()
+# I2C addresses
+DO_ADDR = 100
+PH_ADDR = 99
 
+# Instance of each class that controls the sensor
+device_do = AtlasI2C() 	# For DO
+device_ph = AtlasI2C()	# For pH
+device_dco2 = DCO2()	# For DCO2
+device_brix = BRIX()	# For Brix 
 
-def readAll():
-    do_val = device.query("R")
-    dco2_val = device_dco2.send_read_cmd() 
+# Reading cycle time limit 
+MINIMUM_DELAY = 1.8
+
+def read_all_data():
+    # Get all sensor's data
+    do_val = device_do.query("R")
+    ph_val = device_ph.query("R")
+    dco2_val = 123.0 
     brix_val =[10.0,10.0]
+    #dco2_val = device_dco2.send_read_cmd() 
     #brix_val = device_brix.readData()
-   
-    #timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    timestamp = datetime.now(timezone('Asia/Seoul')).strftime('%Y-%m-%d %H:%M:%S')
-
     
-    data = {'created_time' : timestamp , 'device_id' : DEVICE_ID, 'do' : do_val, 'dco2' : dco2_val, 'brix_temp' : brix_val[0], 'brix_brix': brix_val[1] }    
-   
-    sendAll(data)
-
+    # Send json format data to server
+    timestamp = datetime.now(timezone('Asia/Seoul')).strftime('%Y-%m-%d %H:%M:%S')
+    data = {'created_time' : timestamp , 'device_id' : DEVICE_ID, 'do' : do_val, 'ph' : ph_val, 'dco2' : dco2_val, 'brix_temp' : brix_val[0], 'brix_brix': brix_val[1] }    
+    #send_all_data(data)
+    
+	# Print the data
     print("DO " + do_val + " ppm")
+    print("pH " + ph_val + " ppm")
     print("DCO2 " + str(dco2_val) + " ppm")
     print("Brix-Temp "+ str(brix_val[0]))
     print("Brix-Brix " + str(brix_val[1]) + " %Brix") 
     print("")
 
-def calAll():
-    print("DO done" + device.query("Cal"))
+# Perform remote calibration 
+def calibration_all():
+    # IT MUST BE CHANGED!!!!!!!!!!!!!!!!!!!!
+    print("DO done" + device_do.query("Cal,0"))
     device_dco2.send_cal_cmd()
-    #device_brix.readData()
     print("")
-
-def publish_message(producer, data):
-    
-    try:
-        producer.send(TOPIC_NAME, json.dumps(data).encode('utf-8'))
-        producer.flush()
-
-    except Exception as e:
-        print('Exception in publishing message')
-        print(e)
 
 def connect_kafka_producer():
     _producer = None
@@ -74,72 +74,67 @@ def connect_kafka_producer():
         print(e)
     return _producer 
 
-
-def sendAll(data):
+def send_all_data(data):
     kafka_producer = connect_kafka_producer()
-
-    publish_message(kafka_producer, data)
+    try:
+        kafka_producer.send(TOPIC_NAME, json.dumps(data).encode('utf-8'))
+        kafka_producer.flush()
+    except Exception as e:
+        print('Exception in publishing message')
+        print(e)
     if kafka_producer is not None:
         kafka_producer.close()
 
+def init_setting() :
+    device_do.set_i2c_address(DO_ADDR)
+    device_ph.set_i2c_address(PH_ADDR)
+
 def main():
-    	real_raw_input = vars(__builtins__).get('raw_input', input)
+
+	init_setting()
+	real_raw_input = vars(__builtins__).get('raw_input', input)
 	
 	# main loop
 	while True:
 		user_cmd = real_raw_input("Enter command: ")
+		if user_cmd.upper() =="QUIT" :
+			break
+                        
+		elif user_cmd.upper() =="READ":
+			try:
+				read_all_data()
+			except IOError as e:
+				print("Read failed")
+				print(e)
+			
+		elif user_cmd.upper() =="CAL":
+			try:
+				calibration_all()
+			except IOError as e:
+				print("Calibration failed")
+				print(e)
 
-		if user_cmd.upper().startswith("LIST_ADDR"):
-			devices = device.list_i2c_devices()
-			for i in range(len (devices)):
-				print( devices[i])
-
-		# address command lets you change which address the Raspberry Pi will poll
-		elif user_cmd.upper().startswith("ADDRESS"):
-			addr = int(user_cmd.split(',')[1])
-			device.set_i2c_address(addr)
-			print("I2C address set to " + str(addr))
-
-		# continuous polling command automatically polls the board
-		elif user_cmd.upper().startswith("POLL"):
+		elif user_cmd.upper().startswith("CONT_READ"):
 			delaytime = float(string.split(user_cmd, ',')[1])
 
-			# check for polling time being too short, change it to the minimum timeout if too short
-			if delaytime < AtlasI2C.long_timeout:
-				print("Polling time is shorter than timeout, setting polling time to %0.2f" % AtlasI2C.long_timeout)
-				delaytime = AtlasI2C.long_timeout
+			# Check for polling time being too short
+			# Change it to the minimum delay time if too short
+			if delaytime < MINIMUM_DELAY:
+				print("Polling time is shorter than timeout, setting polling time to %0.2f" % MINIMUM_DELAY)
+				delaytime = MINIMUM_DELAY
 
-			print("%0.2f seconds, press ctrl-c to stop polling" % delaytime)
+			print("%0.2f seconds, press ctrl-c to stop reading" % delaytime)
 
 			try:
 				while True:
-					readAll()
-                                        time.sleep(delaytime - AtlasI2C.long_timeout)
-			except KeyboardInterrupt: 		# catches the ctrl-c command, which breaks the loop above
+					read_all_data()
+					time.sleep(delaytime - MINIMUM_DELAY)
+			except KeyboardInterrupt: 		
+				# Catch the ctrl-c command, which stops reading
 				print("Continuous polling stopped")
-
-		# if not a special keyword, pass commands straight to board
 		else:
-			if len(user_cmd) == 0:
-				print( "Please input valid command.")
+			print( "Please input valid command.")
 			
-                        if user_cmd =="Quit" :
-                            break
-                        
-                        elif user_cmd =="Read":
-				try:
-                                    readAll()
-                                except IOError as e:
-					print("Query failed \n - Address may be invalid, use List_addr command to see available addresses")
-
-                        elif user_cmd =="Cal":
-                    		try:
-                                    calAll()
-                                except IOError as e:
-					print("Query failed \n - Address may be invalid, use List_addr command to see available addresses")
-
-
-
 if __name__ == '__main__':
 	main()
 
