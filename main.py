f727cd59be317d7848f50e6b648b4d978be2bbfa
ina-uzu <ina.uzu.song@gmail.com>
2019-03-05 18:21:47 +0900

    test version

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ from brix import BRIX
 from kafka import KafkaProducer
 import simplejson as json
 
+import requests
+
 DEVICE_ID = 1
 SERVER_ADR = 'localhost:9092'
 TOPIC_NAME = 'hello-kafka'
@@ -37,16 +39,12 @@ device_brix = BRIX()	# For Brix
 MINIMUM_DELAY = 3.0
 
 def read_all_data():
+    '''
     # Get all sensor's data
     do_val = device_do.query("R")
     ph_val = device_ph.query("R")
-
-    #dco2_val = 123.0 
-    #brix_val = device_brix.readData()
-
-    brix_val =[10.0,10.0]
     dco2_val = device_dco2.send_read_cmd() 
-    sg_val = calculate_sg(brix_val)
+    brix_val = device_brix.readData()
 
     # Send json format data to server
     timestamp =str(time.time()).split('.')[0] 
@@ -54,8 +52,21 @@ def read_all_data():
 
     data = {'created_time' : timestamp , 'device_id' : DEVICE_ID, 'do' : do_val, 'ph' : ph_val, 'dco2' : dco2_val, 'brix_temp' : brix_val[0], 'brix_brix': brix_val[1] , 'sg' : sg_val}    
     send_all_data(data)
+
+    '''
     
-	# Print the data
+    ''' Test '''
+    do_val = 12.5
+    ph_val = 4.55
+    dco2_val = 1234
+    brix_val =[10.0,10.0]
+    sg_val = calculate_sg(brix_val)
+
+    data = {'device_id':DEVICE_ID , 'do':do_val, 'ph':ph_val, 'dco2':dco2_val, 'brix_temp':brix_val[0], 'brix_brix':brix_val[1]}
+    URL = 'http://dd966332.ngrok.io/api/data/'
+    reponse = requests.post(URL, data = data)
+    
+    # Print the data
     print("DO " + do_val + " ppm")
     print("pH " + ph_val + " ppm")
     print("DCO2 " + dco2_val + " ppm")
@@ -100,57 +111,56 @@ def init_setting() :
 
 def main():
 
-	init_setting()
-	real_raw_input = vars(__builtins__).get('raw_input', input)
+	#init_setting()
+    real_raw_input = vars(__builtins__).get('raw_input', input)
         
-    print("Welcome!")
     print("1. Read ")
     print("2. Cont_read , n")
     print("3. Cal")
     print("4. Quit")
     print("")
-
+    
 	# main loop
-	while True:
-		user_cmd = real_raw_input("Enter command: ")
-		if user_cmd.upper() =="QUIT"  or user_cmd=="4":
-			break
+    while True:
+        user_cmd = real_raw_input("Enter command: ")
+        if user_cmd.upper() =="QUIT"  or user_cmd=="4":
+            break
                         
-		elif user_cmd.upper() =="READ" or user_cmd=="1":
-			try:
-				read_all_data()
-			except IOError as e:
-				print("Read failed")
-				print(e)
-			
-                elif user_cmd.upper() =="CAL" or user_cmd=="3":
-			try:
-				calibration_all()
-			except IOError as e:
-				print("Calibration failed")
-				print(e)
+        elif user_cmd.upper() =="READ" or user_cmd=="1":
+            try:
+                read_all_data()
+            except IOError as e:
+                print("Read failed")
+                print(e)
 
-		elif user_cmd.upper().startswith("CONT_READ") or user_cmd.startswith("2"):
-                        delaytime = float(user_cmd.split(',')[1])
+        elif user_cmd.upper() =="CAL" or user_cmd=="3":
+            try:
+                calibration_all()
+            except IOError as e:
+                print("Calibration failed")
+                print(e)
+
+        elif user_cmd.upper().startswith("CONT_READ") or user_cmd.startswith("2"):
+            delaytime = float(user_cmd.split(',')[1])
 
 			# Check for polling time being too short
 			# Change it to the minimum delay time if too short
-			if delaytime < MINIMUM_DELAY:
-				print("Polling time is shorter than timeout, setting polling time to %0.2f" % MINIMUM_DELAY)
-				delaytime = MINIMUM_DELAY
+            if delaytime < MINIMUM_DELAY:
+                print("Polling time is shorter than timeout, setting polling time to %0.2f" % MINIMUM_DELAY)
+                delaytime = MINIMUM_DELAY
 
-			print("%0.2f seconds, press ctrl-c to stop reading" % delaytime)
+            print("%0.2f seconds, press ctrl-c to stop reading" % delaytime)
 
-			try:
-				while True:
-					read_all_data()
-					time.sleep(delaytime-MINIMUM_DELAY)
-			except KeyboardInterrupt: 		
-				# Catch the ctrl-c command, which stops reading
-				print("Continuous polling stopped")
-		else:
-			print( "Please input valid command.")
-			
+            try:
+                while True:
+                    read_all_data()
+                    time.sleep(delaytime-MINIMUM_DELAY)
+            except KeyboardInterrupt: 		
+                # Catch the ctrl-c command, which stops reading
+                print("Continuous polling stopped")
+        else:
+            print( "Please input valid command.")
+            
 if __name__ == '__main__':
-	main()
+    main()
 
